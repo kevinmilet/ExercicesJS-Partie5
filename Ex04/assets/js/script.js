ab2e72cc4241064b2cf32883140bebce13f25d6f
kevinmilet <kevin.milet@gmail.com>
2020-11-03 14:28:44 +0100

    exo 4 ok

---
+++ Ex04/assets/js/script.js
@@ -0,0 +1,16 @@
+function changeImage() {
+    // let x = element.getElementById('pict');
+    // let v = x.getAttribute("src");
+    // if(v == "assets/img.img.jpg")
+    //     (v = "assets/img/img2.jpg");
+    // else
+    //     (v = "assets/img/img.jpg");
+    // x.setAttribute("src", v);
+    // document.image.src='assets/img/img2.jpg';
+    document.getElementById("pict").src = "assets/img/img2.jpg"		
+}
+
+function scaleImage() {
+    document.getElementById('pict').style.width='150%';
+
+}
