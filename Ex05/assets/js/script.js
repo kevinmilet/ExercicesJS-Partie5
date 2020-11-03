6c191dfb9cf1cf70bfd61187a3be5c7a3f313d5d
kevinmilet <kevin.milet@gmail.com>
2020-11-03 15:11:23 +0100

    exo 5 ok (pas réussi avec les event listener)

---
+++ Ex05/assets/js/script.js
@@ -0,0 +1,19 @@
+function textPink() {
+    document.querySelector('p').style.color = "pink";
+}
+
+function textBlue() {
+    document.querySelector('p').style.color = "blue";
+}
+
+function textViolet() {
+    document.querySelector('p').style.color = "violet";
+}
+
+function textBold() {
+    document.querySelector('p').style.fontWeight = 'bold';
+}
+
+function textItalic() {
+    document.querySelector('p').style.fontStyle = 'italic';
+}
