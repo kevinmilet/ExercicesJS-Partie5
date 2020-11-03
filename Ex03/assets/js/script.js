62e40f54bc6c1f396776d6e8568d0f9cc1d02a55
kevinmilet <kevin.milet@gmail.com>
2020-11-03 11:32:34 +0100

    exo 3 ok

--- Ex03/assets/js/script.js
+++ Ex03/assets/js/script.js
@@ -1,18 +1,45 @@
-function checkPassword()
-{
-var password = document.getElementById('password').value;
-var passwordConfirm = document.getElementById('passwordConfirm').value;
- 
-if(password == passwordConfirm)
-{
-    document.getElementById('password').className = "pwd green"
-    document.getElementById('passwordConfirm').className = "pwdConfirm green";
+let iName = document.getElementById("name");
+let missName = document.getElementById("missName");
+let vName = new RegExp("[a-zA-Z]")
 
-}
-else
-{
-    document.getElementById('password').className = "pwd red";
-    document.getElementById('passwordConfirm').className = "pwdConfirm red";
+let iMail = document.getElementById("mail");
+let missMail = document.getElementById("missMail");
+let vMail = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
 
+let iAge = document.getElementById("age");
+let missAge = document.getElementById("missAge");
+let vAge = new RegExp("[0-9]")
+
+iName.addEventListener('input', checkName);
+iMail.addEventListener('input', checkMail);
+iAge.addEventListener('input', checkAge);
+
+function checkName() {
+    if (iName.validity.valueMissing) {
+        missName.textContent = 'Champ obligatoire'
+        missName.style.backgroundColor = "red";
+    } else if (vName.test(iName.value) == false) {
+        missName.textContent = 'Ce champ ne doit contenir que des lettres'
+        missName.style.backgroundColor = "red";
+    }
 }
+
+function checkAge() {
+    if (iAge.validity.valueMissing) {
+        missAge.textContent = 'Champ obligatoire'
+        missAge.style.backgroundColor = "red";
+    } else if (vAge.test(iAge.value) == false) {
+        missAge.textContent = 'Ce champ ne doit contenir que des chiffres'
+        missAge.style.backgroundColor = "red";
+    }
+}
+
+function checkMail() {
+    if (iAge.validity.valueMissing) {
+        missMail.textContent = 'Champ obligatoire'
+        missMail.style.backgroundColor = "red";
+    } else if (vMail.test(iMail.value) == false) {
+        missMail.textContent = 'Ce champ doit contenir une adresse email valide'
+        missMail.style.backgroundColor = "red";
+    }
 }
