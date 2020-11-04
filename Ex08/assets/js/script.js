45f7f1f21a85dcb3e74a349c1096da935cf5f530
kevinmilet <kevin.milet@gmail.com>
2020-11-04 13:51:34 +0100

    exo 8 ok

--- Ex08/assets/js/script.js
+++ Ex08/assets/js/script.js
@@ -1,9 +1,15 @@
 function cloneField() {
+    // on assigne une variable à chaque élément du champs Prénom
+    
     let labelName = document.getElementById('label1');
     let fieldName = document.getElementById('firstName');
 
+    // on assigne une variable à chaque élément du champs Age
+
     let labelAge = document.getElementById('label2');
     let fieldAge = document.getElementById('age');
+
+    // on clone les lables et les inputs et on réasigne un nouvel index pour éviter les conflits
 
     let newLabelName = labelName.cloneNode(true);
     let newFirstnameField = fieldName.cloneNode(true);
@@ -14,6 +20,8 @@ function cloneField() {
     let newAgeField = fieldAge.cloneNode(true);
 
     newAgeField.id = 'newAge';
+
+    // on affiche les clones avec les nouveaux indexs
     
     document.body.appendChild(newLabelName);
     document.body.appendChild(newFirstnameField);
