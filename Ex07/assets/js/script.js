43c783ddeb6bd5e2fe393c90cd6e73bad9ecbc3e
kevinmilet <kevin.milet@gmail.com>
2020-11-04 16:59:09 +0100

    modif ex 07

--- Ex07/assets/js/script.js
+++ Ex07/assets/js/script.js
@@ -3,3 +3,9 @@ document.addEventListener('scroll', textFat);
 function textFat() {
     document.querySelector('p').style.fontSize = "x-large";
 }
+
+// let scrolling = document.getElementById('scroller');
+// scrolling.addEventListener('scroll', function(textFat) {
+//     console.log('coucou');
+//     document.querySelector('p').style.fontSize = "x-large";
+// })
