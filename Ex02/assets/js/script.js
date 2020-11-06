9de727be7433f25e4b89b1f7afc006637491d434
kevinmilet <kevin.milet@gmail.com>
2020-11-06 16:30:11 +0100

    modif exo 2 + commentaires

--- Ex02/assets/js/script.js
+++ Ex02/assets/js/script.js
@@ -1,17 +1,19 @@
+// création du listener pour l'execution de la fonction au clic
+const buttonElement = document.getElementById('btn');
+buttonElement.addEventListener('click', checkPassword);
+
+// fonction vérifiant et validant ou non la correspondance des mots de passe
 function checkPassword() {
-let password = document.getElementById('password').value;
-let passwordConfirm = document.getElementById('passwordConfirm').value;
- 
-if(password == passwordConfirm)
-{
-    document.getElementById('password').className = "pwd green"
-    document.getElementById('passwordConfirm').className = "pwdConfirm green";
+    let password = document.getElementById('password').value;
+    let passwordConfirm = document.getElementById('passwordConfirm').value;
 
-}
-else
-{
-    document.getElementById('password').className = "pwd red";
-    document.getElementById('passwordConfirm').className = "pwdConfirm red";
+    if (password == passwordConfirm) {
+        document.getElementById('password').className = "pwd green"
+        document.getElementById('passwordConfirm').className = "pwdConfirm green";
 
-}
+    } else {
+        document.getElementById('password').className = "pwd red";
+        document.getElementById('passwordConfirm').className = "pwdConfirm red";
+
+    }
 }
