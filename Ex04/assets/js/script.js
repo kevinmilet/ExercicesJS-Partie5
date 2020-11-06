05a92ab00e813367db16991cc876ba359879651b
kevinmilet <kevin.milet@gmail.com>
2020-11-06 16:12:19 +0100

    modif exo 4

--- Ex04/assets/js/script.js
+++ Ex04/assets/js/script.js
@@ -1,9 +1,11 @@
 function changeImage() {
-   
     document.getElementById("pict").src = "assets/img/img2.jpg"		
 }
 
 function scaleImage() {
-    document.getElementById('pict').style.width='150%';
+    document.getElementById('pict').style.width='750px';
+}
 
+function unScaleImage() {
+    document.getElementById('pict').style.width='500px';
 }
--- Ex06/assets/js/script.js
+++ Ex06/assets/js/script.js
@@ -1,9 +1,9 @@
 function getFocus(x) {
-    // x.style.outline = "3px solid #000";
-    x.style.outlineColor = "red";
+    x.style.outline = "3px solid #000";
+    // x.style.outlineColor = "red";
 
 }
 
 function lostFocus(x) {
-    x.style.outlineColor = "none";
+    x.style.outline = "none";
 }
