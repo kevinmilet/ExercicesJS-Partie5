de38a6b21d72f10a088564011a2ed233c5f92d2e
kevinmilet <kevin.milet@gmail.com>
2020-11-06 16:21:49 +0100

    modif + ajout commentaires exo 1

--- Ex01/assets/js/script.js
+++ Ex01/assets/js/script.js
@@ -1,18 +1,19 @@
-function checkPassword()
-{
-let password = document.getElementById('password').value;
-let passwordConfirm = document.getElementById('passwordConfirm').value;
- 
-if(password == passwordConfirm)
-{
-    document.getElementById('password').style.borderColor="green";
-    document.getElementById('passwordConfirm').style.borderColor="green";
+// création du listener pour l'execution de la fonction au clic
+const buttonElement = document.getElementById('btn');
+buttonElement.addEventListener('click', checkPassword);
 
-}
-else
-{
-    document.getElementById('password').style.borderColor="red";
-    document.getElementById('passwordConfirm').style.borderColor="red";
+// fonction vérifiant et validant ou non la correspondance des mots de passe
+function checkPassword() {
+    let password = document.getElementById('password').value;
+    let passwordConfirm = document.getElementById('passwordConfirm').value;
 
-}
+    if (password == passwordConfirm) {
+        document.getElementById('password').style.borderColor = "green";
+        document.getElementById('passwordConfirm').style.borderColor = "green";
+
+    } else {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('passwordConfirm').style.borderColor = "red";
+
+    }
 }
