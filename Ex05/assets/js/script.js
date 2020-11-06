8bccecc78d44b4ddce00e75f9614c2839e5201e4
kevinmilet <kevin.milet@gmail.com>
2020-11-06 16:55:42 +0100

    ajout commentaires exo 5

--- Ex05/assets/js/script.js
+++ Ex05/assets/js/script.js
@@ -1,6 +1,7 @@
+// ajout des listeners pour les boutons
 let paragraph = document.querySelector('p');
-let pink = document.getElementById('tPink');
 
+let pink = document.getElementById('tPink');
 pink.addEventListener('click', textPink);
 
 let blue = document.getElementById('tBlue');
@@ -15,23 +16,27 @@ bold.addEventListener('click', textBold);
 let italic = document.getElementById('tItalic');
 italic.addEventListener('click', textItalic);
 
-
+// fonction bouton rose
 function textPink() {
     paragraph.style.color = "pink";
 }
 
+// fonction bouton bleu
 function textBlue() {
     paragraph.style.color = "blue";
 }
 
+// fonction bouton violet
 function textViolet() {
     paragraph.style.color = "violet";
 }
 
+// fonction bouton gras
 function textBold() {
     paragraph.style.fontWeight = 'bold';
 }
 
+// fonction bouton italique
 function textItalic() {
     paragraph.style.fontStyle = 'italic';
 }
