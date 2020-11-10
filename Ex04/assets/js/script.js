2312cae6eb7ad2b83fd250a7d85ef9c4ce67b44f
kevinmilet <kevin.milet@gmail.com>
2020-11-10 14:37:44 +0100

    reup exo 4

--- Ex04/assets/js/script.js
+++ Ex04/assets/js/script.js
@@ -1,19 +1,24 @@
 // mise en place des listeners
-document.addEventListener('click', changeImage);
-document.addEventListener('mouseover', scaleImage);
-document.addEventListener('mouseout', unScaleImage);
+let pict = document.getElementById('pict');
+pict.addEventListener('click', changeImage);
+pict.addEventListener('mouseover', scaleImage);
+pict.addEventListener('mouseout', unScaleImage);
 
 // fonction qui change la source de l'image au clic
 function changeImage() {
-    document.getElementById("pict").src = "assets/img/img2.jpg"		
+    if (pict.getAttribute('src') == 'assets/img/img.jpg') {
+        pict.setAttribute('src', 'assets/img/img2.jpg');		
+    } else {
+        pict.setAttribute('src', 'assets/img/img.jpg');		
+    };
 }
 
-// fontion qui agrandit l'image au survole de la souris
+// fonction qui agrandit l'image au survole de la souris
 function scaleImage() {
-    document.getElementById('pict').style.width='750px';
+    pict.style.width='750px';
 }
 
 // fonction qui fait reprendre la taille normale de l'image quand le pointeur quitte l'image
 function unScaleImage() {
-    document.getElementById('pict').style.width='500px';
+    pict.style.width='500px';
 }
